b09242c1de4a4bb27e4a8ad4cd473cff5cc8ad88
goo-gy <lkukh17@naver.com>
2022-05-14 01:36:42 +0900

    좀더 보기 편하게

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 export default function Home({ $target }) {
   const $page = document.createElement('div');
-  $page.innerHTML =
-    '<div><h1>Home</h1><a href="/page1">page1</a><a href="/page2">page2</a></div>';
+  $page.innerHTML = `
+    <div>
+      <h1>Home</h1>
+      <div><a href="/page1">page1</a></div>
+      <div><a href="/page2">page2</a></div>
+    </div>`;
 
   this.render = () => {
     $target.appendChild($page);
