48bf1df898e9401ce137c342a9ce5722d8a86be8
goo-gy <lkukh17@naver.com>
2022-05-14 02:00:58 +0900

    querySelector & EventListener

--- src/Page2.js
+++ src/Page2.js
@@ -1,8 +1,17 @@
 export default function Page2({ $target }) {
   const $page = document.createElement('div');
-  $page.innerHTML = '<h1>Page2</h1>';
+  $page.innerHTML = `
+  <div>
+    <h1>Page2</h1>
+    <button class="btn1">click me</button>
+  <div>`;
 
   this.render = () => {
     $target.appendChild($page);
   };
+
+  const btn1 = $page.querySelector('.btn1');
+  btn1.addEventListener('click', () => {
+    alert('hi');
+  });
 }
